bd66975ee6618f5641ee4d32f6ab0c06dd2719f5
Melany Segarra <melanyk.segarra@ucuenca.edu.ec>
2024-09-27 18:17:45 -0500

    se comentó todo el código inicial

--- sketch.js
+++ sketch.js
@@ -1,20 +1,20 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(24);
-  background(0);
-  rectMode(CENTER);
-}
+//function setup() {
+//createCanvas(windowWidth, windowHeight);
+//frameRate(24);
+//background(0);
+//rectMode(CENTER);
+//}
 
-function draw() {
-  background(180, random(0, 50), 250, 10);
+//function draw() {
+//background(180, random(0, 50), 250, 10);
 
-  strokeWeight(5);
-  noFill();
-  stroke(mouseY, 187, 180);
-  circle(mouseX + 80, mouseY - 75, mouseX);
+//strokeWeight(5);
+//noFill();
+//stroke(mouseY, 187, 180);
+//circle(mouseX + 80, mouseY - 75, mouseX);
 
-  strokeWeight(1);
-  stroke(15, 25, 50);
-  fill(79, 56, 89);
-  rect(mouseX, mouseY, 80, 80);
-}
+//strokeWeight(1);
+//stroke(15, 25, 50);
+//fill(79, 56, 89);
+//rect(mouseX, mouseY, 80, 80);
+//}
